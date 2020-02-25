15f4f67e3ff940866f157baa1b2c696aad4e209d
asharaloran96 <asharoran96@gmail.com>
2020-02-25 20:19:29 +0200

    Updates

---
+++ precache-manifest.8a699bbaf28f7ec10fee1b4ee2fa53f8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5291c3cdb831265a656cd2f575926c6",
+    "url": "/lab-27-React/index.html"
+  },
+  {
+    "revision": "4490d86e0f3a5ff42435",
+    "url": "/lab-27-React/static/css/main.a0d01d6e.chunk.css"
+  },
+  {
+    "revision": "3a1cfd73315e0531b2ce",
+    "url": "/lab-27-React/static/js/2.37de1627.chunk.js"
+  },
+  {
+    "revision": "21ced859ea2b2d6b856d461ad6c2afed",
+    "url": "/lab-27-React/static/js/2.37de1627.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4490d86e0f3a5ff42435",
+    "url": "/lab-27-React/static/js/main.53c53144.chunk.js"
+  },
+  {
+    "revision": "52aac98bde6e0bab6122",
+    "url": "/lab-27-React/static/js/runtime-main.a76b321d.js"
+  }
+]);
